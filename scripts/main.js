5c85276cc12a7c1f96f95891246f7e4a877cc161
Neil Mispelaar <neil.mispelaar@outlook.com>
2017-03-09 17:58:36 -0500

    Lots of updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
-/*! bootstrap-4-development-box 08-03-2017 */
+/*! bootstrap-4-development-box 09-03-2017 */
 jQuery(document).ready(function(a){a(window).scroll(function(){var b=a(window).scrollTop(),c=a(".navbar");b>20?(c.removeClass("bg-transparent-dark"),c.addClass("bg-primary")):(c.removeClass("bg-primary"),c.addClass("bg-transparent-dark"))})});
